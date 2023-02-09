7f5f43900bfb18b87821667128790e3038ea1422
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2023-02-09 00:08:43 -0500

    integrate gorm sqlite with website
    Hello world using sqlite as our database. The website should pull
    from the test.db file to display on website. Woohoo!

--- behere_project/src/server/main/main.go
+++ behere_project/src/server/main/main.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Location struct {
@@ -18,9 +20,10 @@ type Location struct {
 }
 
 type Person struct {
-	name string
-	age  int
-	loc  Location
+	ID   uint `gorm:"primarykey"`
+	Name string
+	Age  uint
+	// loc  Location
 	// profile picture
 	// friends
 }
@@ -31,7 +34,45 @@ type Event struct {
 	attendees []Person
 }
 
+// Global declaration of the database
+var db *gorm.DB
+
 func main() {
+	var err error
+	/* Database initialization */
+	db, err = gorm.Open(sqlite.Open("src/server/internal/test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(Person{})
+
+	// Create
+	var p1Loc Location
+	p1Loc.x = 50.0
+	p1Loc.y = 100.0
+	p1Loc.z = 50.0
+
+	/*
+		db.Create(&Person{ID: 1, Name: "Golang w GORM Sqlite", Age: 20})
+		db.Create(&Person{ID: 2, Name: "aj", Age: 20})
+		db.Create(&Person{ID: 3, Name: "john", Age: 19})
+		db.Create(&Person{ID: 4, Name: "Nick", Age: 21})
+	*/
+
+	// Read
+	var p1 Person
+	db.First(&p1) // should find person with integer primary key, but just gets first record
+
+	fmt.Print(p1.Name)
+
+	// Update - update person's name
+	//db.Model(&p1).Update("name", "John")
+	// Delete - delete product
+	//db.Delete(&p1, 1)
+
+	/* Server initializaiton */
 	r := mux.NewRouter()
 
 	r.HandleFunc("/hello-world", helloWorld)
@@ -46,15 +87,19 @@ func main() {
 		Handler: handler,
 		Addr:    ":" + os.Getenv("PORT"),
 	}
-
+	//fmt.Print(srv.Addr)
 	log.Fatal(srv.ListenAndServe())
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
+
+	var p2 Person
+	db.First(&p2)
+
 	var data = struct {
 		Title string `json:"title"`
 	}{
-		Title: "Golang Backend",
+		Title: p2.Name,
 	}
 
 	jsonBytes, err := utils.StructToJSON(data)
