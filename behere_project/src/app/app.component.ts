e6750f17ce56c3ea140536469afc1966dca2e57e
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2023-03-29 11:06:35 -0400

    Cleanup appcomponent, moved styles to its css file

--- behere_project/src/app/app.component.ts
+++ behere_project/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { Data } from '@angular/router';
 import {HelloWorldService} from './hello-world.service';
-import { GmapComponent } from './gmap/gmap.component';
 
 
 
