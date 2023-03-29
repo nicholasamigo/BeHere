5840ace53b07ee83156ceed93a0163e779a7c5e0
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2023-03-28 21:31:35 -0400

    Attemping new login solution with Firebase

--- behere_project/src/app/app.module.ts
+++ behere_project/src/app/app.module.ts
@@ -9,38 +9,66 @@ import { AppComponent } from './app.component';
 
 import { GmapModule } from './gmap/gmap.module';
 
-
-import { SocialLoginModule } from 'angularx-social-login';
-import { GoogleLoginProvider } from 'angularx-social-login/public-api';
-//import { Ro }
-
-
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AppFirebaseModule } from './app-firebase.module';
+import { AppMaterialModule } from './app-material.module';
+import { CreateComponent } from './components/create/create.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreateComponent,
+    FeedComponent,
+    ProfileCardComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     GmapModule,
-    SocialLoginModule
+    AppFirebaseModule,
+    AppMaterialModule,
+    RouterModule.forRoot([
+      //{path: '**', component: LoginComponent}
+    ]),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
   // https://medium.com/@danilrabizo/google-authentication-in-the-angular-application-e86df69be58a
-  providers: [HelloWorldService, 
-  {
-    provide: 'SocialAuthServiceConfig',
-    useValue: {
-      autoLogin: true,
-      providers: [
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider('392022857244-ds33po9lfqcqo83mvsovppol0d0001op.apps.googleusercontent.com')
-        }
-      ]
-    }
-  }
-  ],
+  providers: [HelloWorldService, ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- behere_project/src/environments/environment.ts
+++ behere_project/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'behere2-1c02c',
+    appId: '1:248619167492:web:5b323aaf1bf3276a23aa51',
+    storageBucket: 'behere2-1c02c.appspot.com',
+    apiKey: 'AIzaSyDVY8Fb9hjgGZMMPwdbESOWrejNkJKD1HA',
+    authDomain: 'behere2-1c02c.firebaseapp.com',
+    messagingSenderId: '248619167492',
+    measurementId: 'G-W9213FZTS5',
+  },
     production: false,
     serverUrl: 'http://localhost:4201'
   };
