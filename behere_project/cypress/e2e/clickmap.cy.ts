78cb73ff8ef4979ee9c9b11eb09d8e18de0a077c
nicholasamigo <nicholas.amigo@ufl.edu>
2023-03-29 23:37:14 -0400

    Sprint 3

--- behere_project/cypress/e2e/clickmap.cy.ts
+++ behere_project/cypress/e2e/clickmap.cy.ts
@@ -27,4 +27,65 @@ describe('Google Maps Tests', () => {
     // make sure the right hand text updates
     cy.get('#currEventTitle').should('not.have.text', 'Title: NA');
   });
+
+
+  // Sprint 3 Testing
+
+
+  // End-to-End Testing
+
+  // Testing whether the user can login successfully. Testing is done with the new "Create Event" template button to
+  // the left of the "Login" button, and making sure it appears.
+  it('Can successfully login', () => {
+    cy.visit('http://localhost:4200/');
+
+    // Wait for website to load correctly.
+    cy.wait(1500);
+
+    // Click the avatar button used for logging in.
+    // cy.get('*avatar-button').first().click();
+      cy.get('button').first().click();
+
+    // Wait 2 more seconds to successfully log in.
+    cy.wait(2000);
+
+    // cy.get('#createAnEvent').should('be.visible');
+    cy.get('button[id="createAnEvent"]').should('be.visible');
+  });
+
+  // Testing whether the user can logout of the page successfully. Testing is done with the disappearance of the
+  // "Create Event" template button to the left of the "Login" button.
+  it('Can successfully logout', () => {
+    cy.visit('http://localhost:4200/');
+
+    // Wait for website to load correctly.
+    cy.wait(1500);
+
+
+    // Click the avatar button used for logging in.
+    cy.get('.avatar-button').first().click();
+
+    // Wait 2 more seconds to successfully log in.
+    cy.wait(2000);
+
+
+    // Click the avatar button used for logging in.
+    cy.get('.avatar-button').first().click();
+
+    // Wait 2 more seconds to successfully log in.
+    cy.wait(2000);
+
+
+    cy.get('#createAnEvent').should('not.be.visible');
+
+  });
+
+  // Testing whether the user can create a new event with the temporary button. Testing is done with the inclusion of
+  // a new location in the test2.db file.
+  it('Can create an event successfully using the temporary "create event" button', () => {
+    cy.visit('http://localhost:4200/');
+
+    cy.wait(1500);
+  });
+
 });
